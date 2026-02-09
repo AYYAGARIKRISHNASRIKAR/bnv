b5f03f8a4642de8fa7043ea82322a5f80cdb6f53
AYYAGARIKRISHNASRIKAR <153370907+AYYAGARIKRISHNASRIKAR@users.noreply.github.com>
2026-02-09 22:46:30 +0530

    Create api.js

---
+++ frontend/src/services/api.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+});
+
+// USERS API
+export const userAPI = {
+  // Get users (pagination)
+  getUsers: (page = 1, limit = 10) =>
+    api.get(`/users?page=${page}&limit=${limit}`),
+
+  // Search users
+  searchUsers: (query, page = 1, limit = 10) =>
+    api.get(`/users/search?query=${query}&page=${page}&limit=${limit}`),
+
+  // Create user
+  createUser: (data) =>
+    api.post(`/users`, data, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    }),
+
+  // Update user
+  updateUser: (id, data) =>
+    api.put(`/users/${id}`, data, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    }),
+
+  // Delete user
+  deleteUser: (id) =>
+    api.delete(`/users/${id}`),
+
+  // Export CSV
+  exportToCSV: () =>
+    api.get(`/users/export/csv`, {
+      responseType: "blob",
+    }),
+};
+
+export default api;
